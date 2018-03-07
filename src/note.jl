ae936eeffefd00353adeac7d520e6589dfa6c512
dysonance <jamos125@gmail.com>
2018-03-07 07:43:05 -0500

    tests added for note type and smarter note type constructor

--- src/note.jl
+++ src/note.jl
@@ -4,6 +4,11 @@ mutable struct Note
     name::String
     octave::Int
     function Note(name::String="C", octave::Int=4)
+        @assert length(name) < 3 || length(name) == 3 && name[3] in ('0', '1', '2', '3', '4', '5', '6', '7', '8', '9')
+        if length(name) == 3
+            octave = parse(Int, name[3])
+            name = name[1:2]
+        end
         return new(name, octave)
     end
 end
--- test/runtests.jl
+++ test/runtests.jl
@@ -2,4 +2,14 @@ using Stochatto
 using Base.Test
 
 # write your own tests here
-@test 1 == 2
+@testset "Note" begin
+    note = Note()
+    @test note.name == "C"
+    @test note.octave == 4
+    note = Note("Bb", 3)
+    @test note.name == "Bb"
+    @test note.octave == 3
+    note = Note("Eb5")
+    @test note.name == "Eb"
+    @test note.octave == 5
+end
