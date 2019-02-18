ddc00d66df36443c9e7234dca303c6c1d179756e
Jacob Amos <jamos125@gmail.com>
2019-02-18 18:08:12 -0500

    add test dependency and cleanup test cases

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,19 +1,6 @@
 using Stochatto
 using Test
 
-# write your own tests here
-@testset "Note" begin
-    note = Note()
-    @test note.name == "C"
-    @test note.octave == 4
-    note = Note("Bb", 3)
-    @test note.name == "Bb"
-    @test note.octave == 3
-    note = Note("Eb5")
-    @test note.name == "Eb"
-    @test note.octave == 5
-end
-
 @testset "Algorithms" begin
     N = 1000
     R = 0.01:0.01:0.99
